11fb8a8578649b3d273cba2c8e93527ee27d6d41
kingmui <muikinghk@yahoo.com.hk>
2017-10-19 22:56:59 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # HelloFrontEnd
 Front-End basic learning
+
+
+
+[01.H5C3](./01.H5C3.html)
+
+[02.JavaScript基础](./02.JavaScript基础.html)
+
+[03.WebAPI](./03.WebAPI.html)
+
+[04.JS动画](./04.JS动画.html)
+
+[05.JavaScript高级](./05.JavaScript高级.html)
+
+[06.jQuery](./06.jQuery.html)
+
+[07.PHP](./07.PHP.html)
+
+[08.MySQL](./08.MySQL.html)
+
+[09.AJAX](./09.AJAX.html)
+
+[10.Canvas](./10.Canvas.html)
+
+[11.移动Web](./11.移动Web.html)
+
+[my_php_functions](./my_php_functions.php)
--- trans_deal.php
+++ trans_deal.php
@@ -2,15 +2,16 @@
 header("content-type:text/html;charset=utf-8");
 $fileName = $_GET['fileName'];
 // echo realpath($fileName);die;
-$filePath = "C:\\Users\\muiki\\Desktop\\km的笔记\\".$fileName;
+$filePath = "E:\\Code\\GitHub\\HelloFrontEnd\\".$fileName;
 $filePath = iconv("utf-8", "gbk", $filePath);
 $cont = file_get_contents($filePath);
-$lk = "<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,700,400&subset=latin,latin-ext' rel='stylesheet' type='text/css' />";
+// $lk = "<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,700,400&subset=latin,latin-ext' rel='stylesheet' type='text/css' />";
 $md = ".md<";
 $padding = " 100px";
 $anch = "a { cursor: pointer; }";
-if(strpos($cont, $lk) !== false || strpos($cont, $md) !== false || strpos($cont, $padding) !== false || strpos($cont, $anch) !== false){
-	$cont = str_replace($lk, "", $cont);
+// strpos($cont, $lk) !== false || 
+if(strpos($cont, $md) !== false || strpos($cont, $padding) !== false || strpos($cont, $anch) !== false){
+	// $cont = str_replace($lk, "", $cont);
 	$cont = str_replace($md, "<", $cont);
 	$cont = str_replace($padding, "", $cont);
 	$cont = str_replace($anch, "a { cursor: pointer; text-decoration: none;}", $cont);
